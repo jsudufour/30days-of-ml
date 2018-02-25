232a5d97262e111634b7dbc4e65eedc1e69363d4
jsudufour <jsudufour@gmail.com>
2018-02-25 23:33:06 +0000

    prepare data for classification and run initial classification model using knn

--- 1-drugs/PersonalityAndDrugUse-Classification.py
+++ 1-drugs/PersonalityAndDrugUse-Classification.py
@@ -1,29 +1,32 @@
 
 # coding: utf-8
 
-# In[303]:
+# In[56]:
 
 pd.options.display.max_columns = 40
 
 
-# In[304]:
+# In[95]:
 
 # Import libraries
 import pandas as pd
 import numpy as np
-import seaborn as sb
+import seaborn as sns
 from matplotlib import pyplot as plt
 from pandas.tools.plotting import parallel_coordinates
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
 
 
-# In[305]:
+# In[34]:
 
 # Load dataset
 drugs = pd.read_csv('drugs.csv', header = None)
 drugs.head()
 
 
-# In[306]:
+# In[35]:
 
 drugs.columns = ['id', 'age', 'gender', 'education', 'country', 'ethnicity', 'n-score', 'e-score', 'o-score',  
                  'a-score', 'c-score', 'impulsive', 'sensation-seeking', 'alcohol', 'amphetamines', 'amyl-nitrite', 'benzodiapezines', 'caffeine',
@@ -32,7 +35,7 @@ drugs.columns = ['id', 'age', 'gender', 'education', 'country', 'ethnicity', 'n-
 drugs.head()
 
 
-# In[307]:
+# In[36]:
 
 # Dict of usage definitions
 usage = {
@@ -55,12 +58,12 @@ usage_dummy = {
         }
 
 
-# In[308]:
+# In[37]:
 
 drugs.columns
 
 
-# In[309]:
+# In[38]:
 
 # Add drug pleiad groups
 pleiads = {
@@ -82,23 +85,22 @@ pleiads = {
     'methadone': 'benzodiazepines',
     'amphetamines': 'benzodiazepines',
     'coke': 'cocaine',
-    'benzodiapezines': 'benzodiazepines',
+    'benzodiazepines': 'benzodiazepines',
     'alcohol': 'legal highs',
     'caffeine': 'legal highs',
     'chocolate': 'legal highs',
     'amyl nitrite': 'other',
     'vsa': 'other',
     'legal highs': 'legal highs',
-    'semer': 'semer',
+    'semer': 'control',
     'nicotine': 'legal highs',
 }
 
-# 'alcohol', 'amphet', 'amyl', 'benzos', 'caff',
-# 'cannabis', 'choc', 'coke', 'crack', 'ecstasy', 'heroin', 'ketamine',
-# 'legal-h', 'lsd', 'meth', 'mushrooms', 'nicotine', 'semer', 'vsa'
+# cocaine belongs to three pleiads: heroin, ecstasy, benzodiazepines
+# methadone belongs to two pleiads: heroin, benzodiazepines
 
 
-# In[310]:
+# In[39]:
 
 # Ethnicities
 ethnicities = {
@@ -112,7 +114,7 @@ ethnicities = {
     }
 
 
-# In[311]:
+# In[40]:
 
 # Countries
 countries = {
@@ -126,7 +128,7 @@ countries = {
     }
 
 
-# In[312]:
+# In[41]:
 
 # Age
 age = {
@@ -139,7 +141,7 @@ age = {
 }
 
 
-# In[313]:
+# In[42]:
 
 # Gender
 gender = {
@@ -148,7 +150,7 @@ gender = {
 }
 
 
-# In[314]:
+# In[43]:
 
 # Education
 education = {
@@ -164,21 +166,21 @@ education = {
 }
 
 
-# In[315]:
+# In[44]:
 
 # replace ethnicity values in df
 drugs['ethnicity'] = drugs['ethnicity'].replace(ethnicities)
 pd.unique(drugs['ethnicity'])
 
 
-# In[316]:
+# In[45]:
 
 # replace age values in df
 drugs['age'] = drugs['age'].replace(age)
 pd.unique(drugs['age'])
 
 
-# In[317]:
+# In[46]:
 
 # replace gender values in df
 drugs['gender'] = drugs['gender'].replace(gender)
@@ -186,32 +188,32 @@ drugs.head()
 pd.unique(drugs['gender'])
 
 
-# In[318]:
+# In[47]:
 
 # replace education values in df
 drugs['education'] = drugs['education'].replace(education)
 pd.unique(drugs['education'])
 
 
-# In[319]:
+# In[48]:
 
 # replace country values in df
 drugs['country'] = drugs['country'].replace(countries)
 pd.unique(drugs['country'])
 
 
-# In[320]:
+# In[49]:
 
 drugs.head()
 
 
-# In[321]:
+# In[50]:
 
 # Replace usage by values
 drugs.columns
 
 
-# In[322]:
+# In[51]:
 
 # Create dummy user/non-user, split drugs into pleiads
 cols = ['alcohol', 'amphetamines', 'amyl-nitrite',
@@ -222,29 +224,29 @@ drugs[cols] = drugs[cols].replace(usage_dummy)
 drugs.head()
 
 
-# In[323]:
+# In[52]:
 
 # Group drugs by type
 d_types = drugs
 d_types.head()
 
 
-# In[324]:
+# In[53]:
 
 d_types.columns
 
 
-# In[325]:
+# In[54]:
 
 dcols = ['alcohol', 'amphetamines', 'amyl nitrite',
-       'benzodiapezines', 'caffeine', 'cannabis', 'chocolate', 'coke', 'crack',
+       'benzodiazepines', 'caffeine', 'cannabis', 'chocolate', 'coke', 'crack',
        'ecstasy', 'heroin', 'ketamine', 'legal highs', 'lsd', 'methadone',
        'mushrooms', 'nicotine', 'semer', 'vsa']
 new_cols_drugs = [pleiads.get(col_name) for col_name in dcols]
 new_cols_drugs
 
 
-# In[326]:
+# In[55]:
 
 new_cols = ['id', 'age', 'gender', 'education', 'country', 'ethnicity', 'n-score',
        'e-score', 'o-score', 'a-score', 'c-score', 'impulsive',
@@ -255,37 +257,123 @@ d_types.columns = new_cols
 d_types.columns
 
 
-# In[327]:
+# In[57]:
 
 d_types.head()
 
 
-# In[328]:
+# In[58]:
 
 types = d_types.iloc[:,0:13]
 types.head()
 
 
-# In[333]:
+# In[60]:
 
 # Merge columns with same group together, sum values
 #df.groupby(df.columns, axis=1).sum()
-d_types_d = d_types[new_cols_drugs]
+d_types_d = d_types.iloc[:,13:]
 d_types_d = d_types_d.groupby(d_types_d.columns, axis = 1).sum()
 d_types_d.head()
 
 
-# In[334]:
+# In[68]:
 
-drugs_types = pd.concat([types, d_types_d])
+drugs_types = types.merge(d_types_d, right_index = True, left_index = True)
 drugs_types.head()
+
+
+# In[75]:
+
+# Build classifier for each drug group based on big-5 personality scores and impulsive/sensation-seeking scores
+# classifiers: knn, decision tree, random forest, linear discriminant analysis, Gaussian mixture, 
+# probability density function estimation, logistic regression and naive Bayes
+# Convert sums to binaries: user/non-user
+drugs_types['benzodiazepines'] = drugs_types['benzodiazepines'].apply(lambda x: 1 if x > 0 else x)
+drugs_types['cocaine'] = drugs_types['cocaine'].apply(lambda x: 1 if x > 0 else x)
+drugs_types['ecstasy'] = drugs_types['ecstasy'].apply(lambda x: 1 if x > 0 else x)
+drugs_types['heroin'] = drugs_types['heroin'].apply(lambda x: 1 if x > 0 else x)
+drugs_types['legal highs'] = drugs_types['legal highs'].apply(lambda x: 1 if x > 0 else x)
+drugs_types['other'] = drugs_types['other'].apply(lambda x: 1 if x > 0 else x)
+drugs_types['semer'] = drugs_types['semer'].apply(lambda x: 1 if x > 0 else x)
+drugs_types.head()
+
+
+# In[87]:
+
+# KNN
+# Split into test/validate/train sets
+drugs_types = shuffle(drugs_types, random_state = 123)
+train = drugs_types.iloc[0:1000,:]
+validate = drugs_types.iloc[1000:1400,:]
+test = drugs_types.iloc[1400:,:]
+
+
+# In[89]:
+
+# Split into data/labels
+train_data = train.iloc[:,6:13]
+train_labels_benzo = train.iloc[:,13]
+train_labels_cocaine = train.iloc[:,14]
+train_labels_ecstasy = train.iloc[:,15]
+train_labels_heroin = train.iloc[:,16]
+train_labels_lh = train.iloc[:,17]
+train_labels_other = train.iloc[:,18]
+train_labels_semer = train.iloc[:,19]
+#####
+validate_data = validate.iloc[:,6:13]
+validate_labels_benzo = validate.iloc[:,13]
+validate_labels_cocaine = validate.iloc[:,14]
+validate_labels_ecstasy = validate.iloc[:,15]
+validate_labels_heroin = validate.iloc[:,16]
+validate_labels_lh = validate.iloc[:,17]
+validate_labels_other = validate.iloc[:,18]
+validate_labels_semer = validate.iloc[:,19]
+#####
+test_data = test.iloc[:,6:13]
+test_labels_benzo = test.iloc[:,13]
+test_labels_cocaine = test.iloc[:,14]
+test_labels_ecstasy = test.iloc[:,15]
+test_labels_heroin = test.iloc[:,16]
+test_labels_lh = test.iloc[:,17]
+test_labels_other = test.iloc[:,18]
+test_labels_semer = test.iloc[:,19]
+
+
+# In[94]:
+
+train_data.head()
+
+
+# In[109]:
+
+np.sqrt(len(drugs))
+
+
+# In[110]:
+
+# Fit KNN classifier
+knn = KNeighborsClassifier(n_neighbors = 43)
+fit = knn.fit(train_data, train_labels_benzo)
+
+
+# In[111]:
+
+# Evaluate for param tuning (value of k)
+validate = knn.predict(validate_data)
+tp, tn, fp, fn = confusion_matrix(validate, validate_labels_benzo).ravel()
+accuracy = (tp + tn)/(tp + tn + fp + fn)
+specificity = tn/(tn + fp)
+sensitivity = tp/(tp + fn)
+print('accuracy: %.2f' % accuracy)
+print('specificity: %.2f' % specificity)
+print('sensitivity: %.2f' % sensitivity)
 
 
 # In[331]:
 
 ##TODO
-# fix drugs_types df
-# fix merging of columns
+# fix gender binarization
 # Plot big 5 traits in parallel coordinates plot for users vs non-users in each drug group
 # Analyze by gender, age, ethnicity, education level
 
